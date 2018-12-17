c0e135b8f9f6afa7b03a84a320954c0cddcf3064
vijayraghu <vijayasarathypr@gmail.com>
2018-12-17 17:58:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import pymysql
-from flask import Flask, request, Response, make_response, jsonify, url_for, redirect, session
+from flask import Flask, request, Response, make_response, jsonify, url_for, redirect, session, render_template
 import sys
 import requests
 import json
