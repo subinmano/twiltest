141c56dccbe1af60bed204eb879a1c3ba8c6e3b6
vijayraghu <vijayasarathypr@gmail.com>
2019-01-16 13:49:20 +0530

    Update testcaseupload.py

--- testcaseupload.py
+++ testcaseupload.py
@@ -70,7 +70,7 @@ def readTestCasesFromDB():
 	conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
 	cur = conn.cursor()
 	cur.execute("SELECT * FROM ivr_test_case_master")
-	fileContent = """<html><title>IVR test case Execution</title><body><table border="1"><tr><th>Test Case ID</th><th>Test Case Step No</th><th>Action</th><th>Input Type</th><th>Input Value</th><th>Pause</th><col width="150"><th>Expected Prompt</th><th>Expected Prompt Duration</th><th>Min Confidence</th><th>Actual Prompt</th><th>Result</th><th>Recording URL</th><th>Recording duration</th></tr>"""
+	fileContent = """<html><title>IVR test case Execution</title><body><table border="1"><tr><th>Test Case ID</th><th>Test Case Step No</th><th>Action</th><th>Input Type</th><th>Input Value</th><th>Pause</th><th>Expected Prompt</th><th>Expected Prompt Duration</th><th>Min Confidence</th><th>Actual Prompt</th><th>Result</th><th>Recording URL</th><th>Recording duration</th></tr>"""
 	testcaseid=""
 	for r in cur:
 		fileContent =  fileContent + '<tr><td>'+validateString(r[0])+'</td><td>'+validateString(r[1])+'</td><td>'+validateString(r[2])+'</td><td>'+validateString(r[3])+'</td><td>'+validateString(r[4])+'</td><td>'+validateString(r[5])+'</td><td>'+validateString(r[6])+'</td><td>'+validateString(r[7])+'</td><td>'+validateString(r[8])+'</td><td>'+validateString(r[9])+'</td><td>'+validateString(r[10])+'</td><td>'+validateString(r[11])+'</td><td>'+validateString(r[12])+'</td></tr>'
