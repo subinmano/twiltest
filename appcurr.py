443caeea8503820bfae9294b3ce3ae8cb5a1dd58
vijayraghu <vijayasarathypr@gmail.com>
2019-01-16 16:48:12 +0530

    Update appcurr.py

--- appcurr.py
+++ appcurr.py
@@ -8,6 +8,8 @@ import sys
 import requests
 import json
 import urllib
+from jiwer import wer
+from difflib import SequenceMatcher
 # Twilio Helper Library
 from twilio.rest import Client
 from twilio.twiml.voice_response import VoiceResponse, Record, Gather, Say, Dial, Play
@@ -16,6 +18,9 @@ from google.oauth2 import service_account
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
+# Import custom modules
+import transcribe
+import updateresult
 
 #Initiate Flask app
 app = Flask(__name__,template_folder='template')
@@ -32,8 +37,8 @@ databasename = os.environ["databasename"]
 databasehost = os.environ["databasehost"]
 databaseusername = os.environ["databaseusername"]
 databasepassword = os.environ["databasepassword"]
-credentials_dgf = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
+########################################################### Upload Test Case ###########################################################################
 # Render Homepage to upload test cases
 @app.route('/TestCaseUpload')
 def load_TestCaseUploadPage():
@@ -65,8 +70,9 @@ def uploadTestCaseToDB(uploadedFileName):
 			inputpause = TestCaseLine[5]
 			expectedValue = TestCaseLine[6]
 			promptDuration = TestCaseLine[7]
-			query = "INSERT INTO ivr_test_case_master(testcaseid,testcasestepid,action,input_type,input_value,pause_break,expected_value,expected_prompt_duration) values (%s,%s,%s,%s,%s,%s,%s,%s)"	
-			args = (caseID,caseStepID,action,inputType,inputValue,inputpause,expectedValue,promptDuration)
+			expectedconfidence = TestCaseLine[8]
+			query = "INSERT INTO ivr_test_case_master(testcaseid,testcasestepid,action,input_type,input_value,pause_break,expected_value,expected_prompt_duration, expected_confidence) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+			args = (caseID,caseStepID,action,inputType,inputValue,inputpause,expectedValue,promptDuration,expectedconfidence)
 			if i!=0:
 				cur.execute(query,args)
 			else:
@@ -81,13 +87,15 @@ def readTestCasesFromDB():
 	conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
 	cur = conn.cursor()
 	cur.execute("SELECT * FROM ivr_test_case_master")
-	fileContent = """<html><title>IVR test case Execution</title><body><table border="1"><col width="180"><col width="380"><col width="280"><tr><th>Test Case ID</th><th>Test Case Step ID</th><th>Action</th><th>Input Type</th><th>Input Value</th><th>Pause</th><th>Expected value</th><th>Prompt Duration</th><th>Actual Prompt</th><th>Confidence</th><th>Status</th><th>Recording URL</th><th>Recording duration</th></tr>"""
+	fileContent = """<html><title>IVR test case Execution</title><body><table border="1"><tr><th>Testcase ID</th><th>Step No</th><th>Action</th><th>Input Type</th><th>Input Value</th><th>Pause</th><th>Expected Prompt</th><th>Expected Prompt Duration</th><th>Min Confidence</th><th>Actual Prompt</th><th>Result</th><th>Recording URL</th><th>Recording duration</th></tr>"""
+	testcaseid=""
 	for r in cur:
-		fileContent =  fileContent + '<tr><td>'+validateString(r[0])+'</td><td>'+validateString(r[1])+'</td><td>'+validateString(r[2])+'</td><td>'+validateString(r[3])+'</td><td>'+validateString(r[4])+'</td><td>'+validateString(r[5])+'</td><td>'+validateString(r[6])+'</td><td>'+validateString(r[7])+'</td><td>'+validateString(r[8])+'</td><td>'+validateString(r[12])+'</td><td>'+validateString(r[10])+'</td><td>'+validateString(r[13])+'</td><td>'+validateString(r[14])+'</td></tr>'
-	testcaseid=r[0]
+		fileContent =  fileContent + '<tr><td>'+validateString(r[0])+'</td><td>'+validateString(r[1])+'</td><td>'+validateString(r[2])+'</td><td>'+validateString(r[3])+'</td><td>'+validateString(r[4])+'</td><td>'+validateString(r[5])+'</td><td>'+validateString(r[6])+'</td><td>'+validateString(r[7])+'</td><td>'+validateString(r[8])+'</td><td>'+validateString(r[9])+'</td><td>'+validateString(r[10])+'</td><td>'+validateString(r[11])+'</td><td>'+validateString(r[12])+'</td></tr>'
+		testcaseid=r[0]
 	cur.close()
 	conn.close()
-	fileContent = fileContent +'<form action="/ExecuteTestCase?TestCaseId='+testcaseid+'" method="post" enctype="multipart/form-data"><input type="submit" value="Execute Test Case" name="submit"></form></body></html>'
+	#fileContent = fileContent +'<form action="/ExecuteTestCase?TestCaseId='+testcaseid+'" method="post" enctype="multipart/form-data"><input type="submit" value="Execute Test Case" name="submit"></form></body></html>'
+	fileContent = fileContent +'<form action="/ExecuteTestCase?TestCaseId='+testcaseid+'" method="post" enctype="multipart/form-data"><input type="submit" value="Execute Test Case" name="submit"></form>''<form action="/ShowTestResult?TestCaseId='+testcaseid+'" method="post" enctype="multipart/form-data"><input type="submit" value="Show Test Result" name="submit"></form></body></html>'
 	return fileContent
 
 #Validation of testcase upload
@@ -138,31 +146,35 @@ def ExecuteTestCaseUpdateResult():
 	testcaseid = request.values.get("TestCaseId", None)
 	hostname = request.url_root
 	print(hostname)
-	return redirect(hostname + 'start?TestCaseId='+testcaseid+'' + , code=307)
+	return redirect(hostname + 'start?TestCaseId='+testcaseid+'', code=307)
 
 # Show testcase execution result in HTML page
-def ReturnTestCaseHTMLResult(testCaseIDToBePublished):	
+@app.route('/ShowTestResult', methods=['GET','POST'])
+def ShowTestResult():
+	testcaseid = request.values.get("TestCaseId", None)
 	conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
 	cur = conn.cursor()
-	cur.execute("SELECT * FROM ivr_test_case_master")
-	fileContent = """<html><title>IVR test case Execution Result</title><body><table border="1"><col width="180"><col width="380"><col width="280"><tr><th>Test Case ID</th><th>Test Case Step ID</th><th>Action </th> <th>Input Type </th> <th>Input Value </th><th>Pause </th> <th>Expected value</th><th>Prompt Duration </th><th>Actual Prompt</th><th>Confidence</th><th>Status</th><th>Recording URL</th><th>Recording duration</th></tr>"""
+	query = "SELECT * FROM ivr_test_case_master where testcaseid=%s"
+	args = (str(testcaseid))
+	cur.execute(query,args)
+	fileContent = """<html><title>IVR test case Execution</title><body><table border="1"><tr><th>Testcase ID</th><th>Step No</th><th>Action</th><th>Input Type</th><th>Input Value</th><th>Pause</th><th>Expected Prompt</th><th>Expected Prompt Duration</th><th>Min Confidence</th><th>Actual Prompt</th><th>Result</th><th>Recording URL</th><th>Recording duration</th></tr>"""
 	for r in cur:
-		fileContent =  fileContent + '<tr><td>'+validateString(r[0])+'</td><td>'+validateString(r[1])+'</td><td>'+validateString(r[2])+'</td><td>'+validateString(r[3])+'</td><td>'+validateString(r[4])+'</td><td>'+validateString(r[5])+'</td><td>'+validateString(r[6])+'</td><td>'+validateString(r[7])+'</td><td>'+validateString(r[8])+'</td><td>'+validateString(r[12])+'</td><td>'+validateString(r[10])+'</td><td>'+validateString(r[13])+'</td><td>'+validateString(r[14])+'</td></tr>'
+		fileContent =  fileContent + '<tr><td>'+validateString(r[0])+'</td><td>'+validateString(r[1])+'</td><td>'+validateString(r[2])+'</td><td>'+validateString(r[3])+'</td><td>'+validateString(r[4])+'</td><td>'+validateString(r[5])+'</td><td>'+validateString(r[6])+'</td><td>'+validateString(r[7])+'</td><td>'+validateString(r[8])+'</td><td>'+validateString(r[9])+'</td><td>'+validateString(r[10])+'</td><td>'+validateString(r[11])+'</td><td>'+validateString(r[12])+'</td></tr>'
 		print("R3==>"+r[3])
 	cur.close()
 	conn.close()
 	fileContent = fileContent + '</body></html>'
 	return fileContent
   
-#########################################Twilio recording code###############################################
-#Receive the POST request
+############################################################# Record Utterances################################################################
+#Receive the POST request from Execute Test Case
 @app.route('/start', methods=['GET','POST'])
 def start():
 	# Get testcase details as string
 	testcaseid = request.values.get("TestCaseId", None)
 	filename = testcaseid + ".json"
 	with open(filename) as json_file:
-		testCaseJSON = json.loads(json_file)
+		testCaseJSON = json.load(json_file)
 		test_case_id = testCaseJSON["test_case_id"]
 		dnis = testCaseJSON["steps"][currentStepCount]["input_value"]
 		print(dnis, cli)
@@ -177,7 +189,8 @@ def start():
 def record_welcome():
 	response = VoiceResponse()
 	currentTestCaseid=request.values.get("test_case_id", None)
-	response.record(trim="trim-silence", action="/recording?StepNumber=1", timeout="3", playBeep="false", recordingStatusCallback=url_for('.recording_stat', step=[1], currentTestCaseID=[currentTestCaseid], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
+	#response.record(trim="trim-silence", action="/recording?StepNumber=1,TestCaseId=currentTestCaseid", timeout="3", playBeep="false", recordingStatusCallback=url_for('.recording_stat', step=[1], currentTestCaseID=[currentTestCaseid], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
+	response.record(trim="trim-silence", action=url_for('.recording', StepNumber=1, TestCaseId=[currentTestCaseid], _external=True), timeout="3", playBeep="false", recordingStatusCallback=url_for('.recording_stat', step=[1], currentTestCaseID=[currentTestCaseid], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
 	return str(response)
 
 # Twilio/Signalwire functions for record and TTS
@@ -185,9 +198,12 @@ def record_welcome():
 def recording():
 	response = VoiceResponse()
 	currentStepCount= request.values.get("StepNumber", None)
+	testcaseid = request.values.get("TestCaseId", None)
+	print("testcaseid is " + testcaseid)
+	filename = testcaseid + ".json"
 	print("CurrentStepCount is " + currentStepCount)
-	with open('details.json') as json_file:
-		testCaseJSON = json.loads(json_file)
+	with open(filename) as json_file:
+		testCaseJSON = json.load(json_file)
 		currentTestCaseid = testCaseJSON["test_case_id"]
 		print ("Test Case ID ==>"+currentTestCaseid)
 		action = testCaseJSON["steps"][int(currentStepCount)]["action"]
@@ -235,59 +251,14 @@ def recording_stat():
 	RecordingChannels = request.values.get("RecordingChannels", None)
 	RecordingStartTime = request.values.get("RecordingStartTime", None)
 	RecordingSource	= request.values.get("RecordingSource", None)
-	Recognized_text = goog_speech2text(RecordingUrl)
-	#updateResultToDB(RecordingUrl, RecordingDuration, testCaseID, StepNumber)
-	updateResultToDB(RecordingUrl, Recognized_text, testCaseID, StepNumber)
+	Recognized_text = transcribe.goog_speech2text(RecordingUrl)
+	if Recognized_text:
+		#updateResultToDB(RecordingUrl, RecordingDuration, testCaseID, StepNumber)
+		updateresult.updateResultToDB(RecordingUrl, Recognized_text, testCaseID, StepNumber)
 	print("testCaseID==>"+str(testCaseID))
 	print ("RecordingUrl==>"+RecordingUrl+"\nRecognizedText==>"+Recognized_text+"\nStep number==>"+str(StepNumber))
 	return ""
 
-# This function calls Google STT and then returns recognition as text
-#@app.route('/goog_speech2text', methods=['GET', 'POST'])
-def goog_speech2text(RecordingUrl):
-	#Generate Google STT Credentials
-	service_account_info = json.loads(credentials_dgf)
-	credentials = service_account.Credentials.from_service_account_info(service_account_info)
-	# Create Google STT client
-	client = speech.SpeechClient(credentials=credentials)
-	#Create temporary file
-	audiofileNameSplit = RecordingUrl.split("/")
-	audiofile = audiofileNameSplit[len(audiofileNameSplit)-1]
-	urllib.request.urlretrieve(RecordingUrl, audiofile)
-	#Pass the audio to be recognized by Google Speech-To-Text
-	with io.open(audiofile, 'rb') as audio_file:
-		content = audio_file.read()
-	audio = speech.types.RecognitionAudio(content=content)
-	#Set the configuration parameters of the audio file for Google STT
-	config = speech.types.RecognitionConfig(
-		encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-		sample_rate_hertz=8000,
-		language_code='en-US',
-		# Enhanced models are only available to projects that opt in for audio data collection
-		use_enhanced=True,
-		# Specify the model for the enhanced model usage.
-		model='phone_call')
-	#Get the response from Google STT	
-	response = client.recognize(config, audio)
-	for result in response.results:
-		print('Transcript: {}'.format(result.alternatives[0].transcript))
-		recognized_text = result.alternatives[0].transcript
-	return recognized_text
-	
-# Update recording metadata to Database
-def updateResultToDB(recordingURL,recognizedText,testcaseID,testCaseStep):
-	conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
-	cur = conn.cursor()
-	print(str(recordingURL)+"||"+str(recognizedText)+"||"+testcaseID+"||"+testCaseStep)
-	query = "UPDATE ivr_test_case_master set recording_url = %s, actual_value = %s where testcaseid=%s and testcasestepid = %s"
-	args = (recordingURL,str(recognizedText),str(testcaseID),testCaseStep)
-	cur.execute(query,args)
-	print("Rows Affected==>"+str(cur.rowcount))
-	conn.commit()
-	cur.close()
-	conn.close()
-	return ""
-  
 if __name__ == '__main__':
 	port = int(os.getenv('PORT', 5000))
 	print ('Starting app on port %d' % port)
