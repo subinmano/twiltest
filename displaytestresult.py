0f3b3bf48b252a8ed5b0c9fbf5af424b35ea0a0b
vijayraghu <vijayasarathypr@gmail.com>
2019-02-11 15:33:27 +0530

    Create displaytestresult.py

---
+++ displaytestresult.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+### This module will display the test case results once the test case has been executed completely
+
+import os
+import io
+import pymysql
+from flask import Flask, request, Response, make_response, jsonify, url_for, redirect, session, render_template
+import sys
+import requests
+import json
+import urllib
+
+#Initiate Flask app
+app = Flask(__name__,template_folder='template')
+
+#Set key for session variables
+SECRET_KEY = os.environ["SECRET_KEY"]
+app.secret_key=SECRET_KEY
+
+# Declare global variables
+databasename = os.environ["databasename"]
+databasehost = os.environ["databasehost"]
+databaseusername = os.environ["databaseusername"]
+databasepassword = os.environ["databasepassword"]
+
+# Show testcase execution result in HTML page
+@app.route('/ShowTestResult', methods=['GET','POST'])
+def ShowTestResult():
+  testcaseid = request.values.get("TestCaseId", None)
+	conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
+	cur = conn.cursor()
+	query = "SELECT * FROM ivr_test_case_master where testcaseid=%s"
+	args = (str(testcaseid))
+	cur.execute(query,args)
+	fileContent = """<html><title>IVR test case Execution</title><body><table border="1"><tr><th>Testcase ID</th><th>Step No</th><th>Action</th><th>Input Type</th><th>Input Value</th><th>Pause</th><th>Expected Prompt</th><th>Expected Prompt Duration</th><th>Min Confidence</th><th>Actual Prompt</th><th>Result</th><th>Recording URL</th><th>Recording duration</th></tr>"""
+	for r in cur:
+		fileContent =  fileContent + '<tr><td>'+validateString(r[0])+'</td><td>'+validateString(r[1])+'</td><td>'+validateString(r[2])+'</td><td>'+validateString(r[3])+'</td><td>'+validateString(r[4])+'</td><td>'+validateString(r[5])+'</td><td>'+validateString(r[6])+'</td><td>'+validateString(r[7])+'</td><td>'+validateString(r[8])+'</td><td>'+validateString(r[9])+'</td><td>'+validateString(r[10])+'</td><td>'+validateString(r[11])+'</td><td>'+validateString(r[12])+'</td></tr>'
+		print("R3==>"+r[3])
+	cur.close()
+	conn.close()
+	fileContent = fileContent + '</body></html>'
+	return fileContent
+  
+if __name__ == '__main__':
+	port = int(os.getenv('PORT', 5000))
+	print ('Starting app on port %d' % port)
+	app.run(debug=False, port=port, host='0.0.0.0')
