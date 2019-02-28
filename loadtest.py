13437c408f316730b7d00e82731230d49f3e2618
vijayraghu <vijayasarathypr@gmail.com>
2019-02-28 16:43:32 +0530

    Update loadtest.py

--- loadtest.py
+++ loadtest.py
@@ -1,1 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import io
+import pymysql
+from flask import Flask, request, Response, make_response, jsonify, url_for, redirect, session, render_template
+import sys
+import requests
+import json
+import urllib
+from jiwer import wer
+from difflib import SequenceMatcher
+from datetime import datetime
+import time
+# Twilio Helper Library
+from twilio.rest import Client
+from twilio.twiml.voice_response import VoiceResponse, Record, Gather, Say, Dial, Play
+# Signalwire Helper lirary
+from signalwire.rest import Client as signalwire_client
+from signalwire.voice_response import VoiceResponse
+# Import custom modules
+import transcribe
+import updateresult
+
+#Initiate Flask app
+app = Flask(__name__,template_folder='template')
+
+#Set key for session variables
+SECRET_KEY = os.environ["SECRET_KEY"]
+app.secret_key=SECRET_KEY
+
+# Declare global variables
+cli = os.environ["cli"]
+account_sid = os.environ["account_sid"]
+auth_token = os.environ["auth_token"]
+signalwire_space_url = os.environ["signalwire_space_url"]
+databasename = os.environ["databasename"]
+databasehost = os.environ["databasehost"]
+databaseusername = os.environ["databaseusername"]
+databasepassword = os.environ["databasepassword"]
+
+
+
+
 
