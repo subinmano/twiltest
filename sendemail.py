3c30b6aa53c8a9ab1bc644cda1cbbda210901bd0
vijayraghu <vijayasarathypr@gmail.com>
2019-03-04 09:10:29 +0530

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -7,21 +7,24 @@ sender_email_id = os.environ["fromemailid"]
 sender_email_id_password = os.environ["frompassword"]
 receiver_email_id = os.environ["toemailid"]
 
-def sendEMAIL(testcaseID, testCaseStep):
+def sendEMAIL(testcaseID, testCaseStep, expected_value, actual_value):
+ 
   # creates SMTP session
-  s = smtplib.SMTP('smtp.gmail.com', 587) 
+  s = smtplib.SMTP('smtp.gmail.com', 587)
   
-# start TLS for security 
-s.starttls() 
+  # start TLS for security
+  s.starttls()
   
-# Authentication 
-s.login("sender_email_id", "sender_email_id_password") 
+  # Authentication
+  s.login("sender_email_id", "sender_email_id_password") 
   
-# message to be sent 
-message = "Message_you_need_to_send"
+  # message to be sent
+  message = 'Please note that Test case number ' +testcaseID+ ' has failed at step ' +testCaseStep+ '. The expected response was ' +expected_value+ ' and the actual response was ' +actual_value+ '.'
   
-# sending the mail 
-s.sendmail("sender_email_id", "receiver_email_id", message) 
+  # sending the mail
+  s.sendmail("sender_email_id", "receiver_email_id", message)
   
-# terminating the session 
-s.quit() 
+  # terminating the session
+  s.quit()
+  
+  return ""
