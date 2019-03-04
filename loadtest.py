32b304e246d52b6f9d8a5738be4b27a3581572cf
vijayraghu <vijayasarathypr@gmail.com>
2019-03-04 14:21:04 +0530

    Update loadtest.py

--- loadtest.py
+++ loadtest.py
@@ -240,8 +240,8 @@ def recording():
 		execution_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 		conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
 		cur = conn.cursor()
-		query = "UPDATE ivr_test_case_master set execution_status = %s, execution_datetime = %s where testcaseid = %s and action = %s"
-		args = (str(execution_status), str(execution_datetime), 'Hangup')
+		query = "UPDATE ivr_load_test_case_master set count = %s, execution_status = %s, execution_datetime = %s where testcaseid = %s"
+		args = (str(execution_status), str(execution_datetime))
 		cur.execute(query,args)
 	return str(response)
 
@@ -252,12 +252,12 @@ def ShowTestResult():
 	testcaseid = request.values.get("TestCaseId", None)
 	conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
 	cur = conn.cursor()
-	query = "SELECT * FROM ivr_test_case_master where testcaseid=%s"
+	query = "SELECT * FROM ivr_load_test_case_master where testcaseid=%s"
 	args = (str(testcaseid))
 	cur.execute(query,args)
 	fileContent = """<html><title>IVR test case Execution Result</title><body><table border="1"><tr><th>Testcase ID</th><th>Step No</th><th>Action</th><th>Input Type</th><th>Input Value</th><th>Pause</th><th>Expected Prompt</th><th>Expected Prompt Duration</th><th>Min Confidence</th><th>Actual Prompt</th><th>Result</th><th>Recording URL</th><th>Recording duration</th><th>Uploaded date</th><th>Execution status</th><th>Execution date</th></tr>"""
 	for r in cur:
-		fileContent =  fileContent + '<tr><td>'+validateString(r[0])+'</td><td>'+validateString(r[1])+'</td><td>'+validateString(r[2])+'</td><td>'+validateString(r[3])+'</td><td>'+validateString(r[4])+'</td><td>'+validateString(r[5])+'</td><td>'+validateString(r[6])+'</td><td>'+validateString(r[7])+'</td><td>'+validateString(r[8])+'</td><td>'+validateString(r[9])+'</td><td>'+validateString(r[10])+'</td><td>'+validateString(r[11])+'</td><td>'+validateString(r[12])+'</td><td>'+validateString(r[13])+'</td><td>'+validateString(r[14])+'</td><td>'+validateString(r[15])+'</td></tr>'
+		fileContent =  fileContent + '<tr><td>'+validateString(r[0])+'</td><td>'+validateString(r[1])+'</td><td>'+validateString(r[2])+'</td></tr>'
 		print("R3==>"+r[3])
 	cur.close()
 	conn.close()
