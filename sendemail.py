48e8f6895e08b7918825af3304f178be93138645
vijayraghu <vijayasarathypr@gmail.com>
2019-03-04 08:51:16 +0530

    Create sendemail.py

---
+++ sendemail.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import smtplib
+
+# Declare global variables
+sender_email_id = os.environ["fromemailid"]
+sender_email_id_password = os.environ["frompassword"]
+receiver_email_id = os.environ["toemailid"]
+
+def sendEMAIL():
+
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login("sender_email_id", "sender_email_id_password") 
+  
+# message to be sent 
+message = "Message_you_need_to_send"
+  
+# sending the mail 
+s.sendmail("sender_email_id", "receiver_email_id", message) 
+  
+# terminating the session 
+s.quit() 
