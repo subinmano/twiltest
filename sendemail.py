af0e90c3ff2d9b359cbf60f3650a1dd713d979b6
vijayraghu <vijayasarathypr@gmail.com>
2019-03-04 09:56:25 +0530

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # Declare global variables
 sender_email_id = os.environ["fromemailid"]
@@ -15,14 +17,26 @@ def sendEMAIL(testcaseID, testCaseStep, expected_value, actual_value):
   # start TLS for security
   s.starttls()
   
-  # Authentication
-  s.login("sender_email_id", "sender_email_id_password") 
+  # mail account credentials
+  s.login("sender_email_id", "sender_email_id_password")
+  
+  # create the message
+  msg = MIMEMultipart()
+  
+  # setup the parameters of the message
+  msg['From']=MY_ADDRESS
+  msg['To']=email
+  msg['Subject']="Test Case failure"
   
   # message to be sent
   message = 'Please note that Test case number ' +testcaseID+ ' has failed at step ' +testCaseStep+ '. The expected response was ' +expected_value+ ' and the actual response was ' +actual_value+ '.'
   
-  # sending the mail
-  s.sendmail("sender_email_id", "receiver_email_id", message)
+  # load the message
+  msg.attach(MIMEText(message, 'plain'))
+  
+  # send the mail
+  s.send_message(msg)
+  del msg
   
   # terminating the session
   s.quit()
