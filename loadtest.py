bf11f940bc7760dd62d979eda2ce701fd0b153b7
vijayraghu <vijayasarathypr@gmail.com>
2019-03-05 09:14:36 +0530

    Update loadtest.py

--- loadtest.py
+++ loadtest.py
@@ -93,7 +93,7 @@ def readTestCasesFromDB():
 		testcaseid=r[0]
 	cur.close()
 	conn.close()
-	fileContent = fileContent +'<form action="/ExecuteTestCase?TestCaseId='+testcaseid+'" method="post" enctype="multipart/form-data"><input type="text" name="numberofcalls" value="Number of calls"><input type="submit" value="Execute Test Case" name="submit"></form>'</form></body></html>'
+	fileContent = fileContent +'<form action="/ExecuteTestCase?TestCaseId='+testcaseid+'" method="post" enctype="multipart/form-data"><input type="text" name="numberofcalls" value="Number of calls"><input type="submit" value="Execute Test Case" name="submit"></form></body></html>'
 	return fileContent
 
 #Validation of testcase upload
