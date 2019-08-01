0107fb40deff6f4d1839ef1c91f2218bd71f474e
vijayraghu <vijayasarathypr@gmail.com>
2019-08-01 21:31:54 +0530

    Update usersequential.py

--- usersequential.py
+++ usersequential.py
@@ -66,11 +66,11 @@ def login_post():
 	remember = True if request.form.get('remember') else False
 	# check if user actually exists , take the user supplied password, hash it, and compare it to the hashed password in database
 	user = User.query.filter_by(email=email).first()
-	if not user or not check_password_hash(user.password, password): 
-        	flash('Please check your login details and try again.')
+	if not user or not check_password_hash(user.password, password):
+		flash('Please check your login details and try again.')
 		#if user doesn't exist or password is wrong, reload the page
 		return redirect(url_for('auth.login'))
-	#if the above check passes, then we know the user has the right credentials
+	#if the above check passes, then we know the user has right credentials
 	login_user(user, remember=remember)
 	return redirect(url_for('main.profile'))
   
