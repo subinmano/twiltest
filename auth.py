cb9c6ba4a52f8207012108f6600205e8a533a004
vijayraghu <vijayasarathypr@gmail.com>
2019-08-06 11:51:59 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -329,8 +329,8 @@ def record_welcome():
 	currentTestCaseid=request.values.get("test_case_id", None)
 	prompt_duration=request.values.get("prompt_duration", '')
 	username=request.values.get("user_name", '')
-	#response.record(trim="trim-silence", action=url_for('.recording', StepNumber=1, TestCaseId=[currentTestCaseid], user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=prompt_duration, recordingStatusCallback=url_for('.recording_stat', step=[1], currentTestCaseID=[currentTestCaseid], user_name=[username], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
-	response.record(trim="trim-silence", action=url_for('.recording', StepNumber=1, TestCaseId=[currentTestCaseid], user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=prompt_duration)
+	response.record(trim="trim-silence", action=url_for('.recording', StepNumber=1, TestCaseId=[currentTestCaseid], user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=prompt_duration, recordingStatusCallback=url_for('.recording_stat', step=[1], currentTestCaseID=[currentTestCaseid], user_name=[username], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
+	#response.record(trim="trim-silence", action=url_for('.recording', StepNumber=1, TestCaseId=[currentTestCaseid], user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=prompt_duration)
 	return str(response)
 
 # Twilio/Signalwire functions for record and TTS
@@ -412,15 +412,15 @@ def recording():
 			currentStepCount=int(currentStepCount)+1
 			print("I am at DTMF input step:: " +str(currentStepCount))
 			response.play(digits=input_value)
-			#response.record(trim="trim-silence", action=url_for('.recording', StepNumber=[str(currentStepCount)], TestCaseId=[currentTestCaseid],user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=max_rec_length, recordingStatusCallback=url_for('.recording_stat', step=[str(currentStepCount)], currentTestCaseID=[currentTestCaseid], user_name=[username], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
-			response.record(trim="trim-silence", action=url_for('.recording', StepNumber=[str(currentStepCount)], TestCaseId=[currentTestCaseid],user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=max_rec_length)
+			response.record(trim="trim-silence", action=url_for('.recording', StepNumber=[str(currentStepCount)], TestCaseId=[currentTestCaseid],user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=max_rec_length, recordingStatusCallback=url_for('.recording_stat', step=[str(currentStepCount)], currentTestCaseID=[currentTestCaseid], user_name=[username], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
+			#response.record(trim="trim-silence", action=url_for('.recording', StepNumber=[str(currentStepCount)], TestCaseId=[currentTestCaseid],user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=max_rec_length)
 			
 		if "Say" in input_type:
 			currentStepCount=int(currentStepCount)+1
 			print("I am at Say input step:: " +str(currentStepCount))
 			response.say(input_value, voice="alice", language="en-US")
-			#response.record(trim="trim-silence", action=url_for('.recording', StepNumber=[str(currentStepCount)], TestCaseId=[currentTestCaseid],user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=max_rec_length, recordingStatusCallback=url_for('.recording_stat', step=[str(currentStepCount)], currentTestCaseID=[currentTestCaseid], user_name=[username], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
-			response.record(trim="trim-silence", action=url_for('.recording', StepNumber=[str(currentStepCount)], TestCaseId=[currentTestCaseid],user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=max_rec_length)
+			response.record(trim="trim-silence", action=url_for('.recording', StepNumber=[str(currentStepCount)], TestCaseId=[currentTestCaseid],user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=max_rec_length, recordingStatusCallback=url_for('.recording_stat', step=[str(currentStepCount)], currentTestCaseID=[currentTestCaseid], user_name=[username], _scheme='https', _external=True),recordingStatusCallbackMethod="POST")
+			#response.record(trim="trim-silence", action=url_for('.recording', StepNumber=[str(currentStepCount)], TestCaseId=[currentTestCaseid],user_name=[username],_external=True), timeout="3", playBeep="false", maxLength=max_rec_length)
 	
 	if "Hangup" in action:
 		print ("I am at hangup")
@@ -443,30 +443,30 @@ def recording():
 @auth.route("/recording_stat", methods=['GET', 'POST'])
 def recording_stat():
 	print("I am at recording callback event")
-	req = request.get_json(silent=True, force=True)
-	print(req)
 	StepNumber = request.values.get("step", None)
 	print("StepNumber==>"+str(StepNumber))
 	testCaseID = request.values.get("currentTestCaseID", None)
 	print("testCaseID==>"+str(testCaseID))
 	username = request.values.get("user_name", None)
 	print("Username==>"+username)
-	RecordingUrl = request.values.get("RecordingUrl", None)
-	print(RecordingUrl)
-	AccountSid = request.values.get("AccountSid", None)
-	CallSid =  request.values.get("CallSid", None)
-	RecordingSid = request.values.get("RecordingSid", None)
 	RecordingStatus = request.values.get("RecordingStatus", None)
-	RecordingDuration = request.values.get("RecordingDuration", None)
-	RecordingChannels = request.values.get("RecordingChannels", None)
-	RecordingStartTime = request.values.get("RecordingStartTime", None)
-	RecordingSource	= request.values.get("RecordingSource", None)
-	Recognized_text = transcribe.goog_speech2text(RecordingUrl)
-	if Recognized_text:
-		updateresult.updateResultToDB(RecordingUrl, Recognized_text, testCaseID, StepNumber,username)
-	print("testCaseID==>"+str(testCaseID))
+	print("Recording status==>"+RecordingStatus)
+	if RecordingStatus == "completed":
+		RecordingUrl = request.values.get("RecordingUrl", None)
+		print("Recording URL==>"+RecordingUrl)
+		RecordingDuration = request.values.get("RecordingDuration", None)
+		print("Recording duration==>"+RecordingDuration)
+	#AccountSid = request.values.get("AccountSid", None)
+	#CallSid =  request.values.get("CallSid", None)
+	#RecordingSid = request.values.get("RecordingSid", None)
+	#RecordingStartTime = request.values.get("RecordingStartTime", None)
+	#RecordingSource = request.values.get("RecordingSource", None)
+	#Recognized_text = transcribe.goog_speech2text(RecordingUrl)
+	#if Recognized_text:
+		#updateresult.updateResultToDB(RecordingUrl, Recognized_text, testCaseID, StepNumber,username)
+	#print("testCaseID==>"+str(testCaseID))
 	
-	print ("RecordingUrl==>"+RecordingUrl+"\nRecognizedText==>"+Recognized_text+"\nStep number==>"+str(StepNumber)+"\nUser name==>"+username)
+	#print ("RecordingUrl==>"+RecordingUrl+"\nRecognizedText==>"+Recognized_text+"\nStep number==>"+str(StepNumber)+"\nUser name==>"+username)
 	return()
 
 if __name__ == '__main__':
